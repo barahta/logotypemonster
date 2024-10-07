1111d9185b6e9cf79eaefd455aa39c780de3c436
Vladimir Barakhtyanskiy <barahtasurgut@gmail.com>
2024-10-07 16:18:25 +0500

    3423

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ClientPage from './pages/Main.js';
 import Main from "./pages/Main.js";
-import AllNews from "./pages/AllNews";
-import Vakansii from "./pages/Vakansii";
-import Contacts from "./pages/Contacts";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
 import {observer} from "mobx-react-lite";
-import Actives from "./pages/Actives";
 
 
 function App() {
@@ -20,10 +16,6 @@ function App() {
         <Routes>
 
             <Route path="/" element={<Main />} />
-            <Route path="/allnews" element={<AllNews />} />
-            <Route path="/vakansii" element={<Vakansii />} />
-            <Route path="/contacts" element={<Contacts />} />
-            <Route path="/activegroup" element={<Actives />} />
         </Routes>
           <ToastContainer />
       </Router>
--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,14 +1,4 @@
 import style from './styles/Main.module.scss'
-import HeaderMain from "../components/header/HeaderMain";
-import AboutUs from "../components/about/AboutUs";
-import NewsBlock from "../components/news/NewsBlock";
-import Projects from "../components/projects/Projects";
-import NewHeader from "../components/newheader/NewHeader";
-import TwoBlocks from "../components/animation/TwoBlocks";
-import Group from "../components/groupcompany/Group";
-import NewAbout from "../components/newabout/NewAbout";
-import Actives from "../components/activeholding/Actives";
-import Footer from "../components/footer/Footer";
 import {useEffect} from "react";
 import {useLocation} from "react-router-dom";
 
@@ -17,25 +7,30 @@ function Main () {
 
     return (
         <div className={style.bodymain}>
-            <NewHeader />
-            <TwoBlocks />
-            <Group />
-            <NewAbout />
-            <Actives />
-            <NewsBlock />
-            <Footer />
-            {/*<HeaderMain />*/}
-            {/*<div className={style.blockvideo}>*/}
-            {/*    <video autoPlay="autoplay" muted="muted" loop="loop" playsInline="">*/}
-            {/*    <source src="/files/header/intro.mp4" type="video/mp4"/>*/}
-            {/*</video>*/}
-
-            {/*</div>*/}
-            {/*<AboutUs />*/}
-            {/*<Projects />*/}
-            {/*<NewsBlock />*/}
-
-
+            <div className={style.container}>
+                <div className={style.title}>
+                    <img src="/files/tooth.svg" className={style.tooth}/>
+                    <div className={style.left}>LOGOTYPE</div>
+                    <div className={style.right}>MONSTER</div>
+                </div>
+                <div className={style.engtitle}>
+                    <div className={style.text}>
+                        <div className={style.line}>LOGO</div>
+                        <div className={style.linedown}>Design</div>
+                    </div>
+                    <div className={style.logo}>
+                        <img src="/files/monster.svg" alt=""/>
+                    </div>
+                </div>
+                <div className={style.rutitle}>СДЕЛАЮ ЛОГОТИП</div>
+                <div className={style.more}>
+                    <div className={style.btn}>Подробнее</div>
+                </div>
+                <div className={style.portfolio}>
+                    <div className={style.eng}>PORTFOLIO</div>
+                    <div className={style.rus}>ПОРТФОЛИО</div>
+                </div>
+            </div>
         </div>
     )
 }
